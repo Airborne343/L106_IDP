6165cf2c799ced2dd66f35d10420c47b2a818ff3
ShaiqAhmed17 <123759522+ShaiqAhmed17@users.noreply.github.com>
2024-01-30 13:38:33 +0000

    Update Dijkstra's Algorithm.cpp

--- Dijkstra's Algorithm.cpp
+++ Dijkstra's Algorithm.cpp
@@ -1,27 +1,33 @@
-#include <iostream>
-#include <string>
-using namespace std;
-#define INF 999
+
 //DIJKSTRA
 
-int V, source, cost[100][100];
+int INF = 999;
+int V = 14; 
+int source = 1;
+int cost[100][100];
 int dist[100];
-bool visited[100] = {0}; 
+bool visited[100] = {0};
 int parent[100];
 
-void init(){
-    for (int i = 0; i < V; i++){
+void setup(){
+  Serial.begin(9600);
+}
+
+
+void init() {
+    for (int i = 0; i < V; i++) {
+        Serial.print("S");
         parent[i] = i;
         dist[i] = INF;
     }
     dist[source] = 0;
 }
 
-int getNearestNode(){
+int getNearestNode() {
     int minvalue = INF;
-    int minnode  = 0;
-    for (int i = 0; i < V; i++){
-        if(!visited[i] && dist[i] < minvalue){
+    int minnode = 0;
+    for (int i = 0; i < V; i++) {
+        if (!visited[i] && dist[i] < minvalue) {
             minvalue = dist[i];
             minnode = i;
         }
@@ -29,63 +35,66 @@ int getNearestNode(){
     return minnode;
 }
 
-void dijkstra(){
-    for (int i = 0; i < V; i++){
+void dijkstra() {
+    for (int i = 0; i < V; i++) {
         int nearest = getNearestNode();
         visited[nearest] = true;
-        for (int adj = 0; adj < V; adj++){
-            if(cost[nearest][adj] != INF && dist[adj] > dist[nearest] + cost[nearest][adj]){
+        for (int adj = 0; adj < V; adj++) {
+            if (cost[nearest][adj] != INF && dist[adj] > dist[nearest] + cost[nearest][adj]) {
                 dist[adj] = dist[nearest] + cost[nearest][adj];
                 parent[adj] = nearest;
-            } 
+            }
         }
     }
 }
 
-void display(){
-    cout<<"Node:\t\t\tCost :\t\t\tPath";
-
-    for(int i = 0; i < V ; i++){
-        cout<<i<<"\t\t\t"<< dist[i]<<"\t\t\t"<<" ";
-        cout<< i <<" ";
+void display() {
+    Serial.println("Node:\t\t\tCost :\t\t\tPath");
+    for (int i = 0; i < V; i++) {
+        Serial.println(i);
+        Serial.print("\t\t\t");
+        Serial.print(dist[i]);
+        Serial.print("\t\t\t");
+        Serial.println( i , " ");
         int parnode = parent[i];
 
-        while (parnode != source){
-            cout<<" <- "<< parnode <<" ";
+        while (parnode != source) {
+            Serial.println(" <- ");
+            Serial.print(parnode);
             parnode = parent[parnode];
         }
-        cout<<endl;
     }
 }
 
-int main(void){
-    cout<<"Number of vertices : ";
-    cin>>V;
-    for (int i = 0; i < V; i++){
-        for (int j = 0 ; j < V; j++){
-            cin >> cost[i][j];
+int main(void) {
+    Serial.println("Number of vertices:");
+    Serial.print(V);
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            Serial.print(cost[i][j]);
         }
     }
-    cout<<"src node : ";
-    cin>>source;
+    Serial.println("src node : ");
+    Serial.print(source);
+    setup();
     init();
     dijkstra();
-    display(); 
+    display();
 }
 
 /*
-0     5     999   999   999   999   999   999   999   999   999   999   999   999
-5     0     5     999   999   999   5     999   999   999   999   999   999   999
-999   5     0     5     5     999   999   999   999   999   999   999   999   999
-999   999   5     0     999   999   999   999   999   999   999   999   999   999
-999   999   5     999   0     5     999   999   5     999   999   999   999   999
-999   999   999   999   5     0     999   999   999   999   999   999   999   999
-999   5     999   999   999   999   0     5     999   5     999   999   999   999
-999   999   999   999   999   999   5     0     999   999   999   999   999   999
-999   999   999   999   5     999   999   999   0     999   999   999   5     5  
-999   999   999   999   999   999   5     999   999   0     5     999   999   5  
-999   999   999   999   999   999   999   999   999   5     0     5     5     999
-999   999   999   999   999   999   999   999   999   999   5     0     999   999
-999   999   999   999   999   999   999   999   5     999   5     999   0     5  
-999   999   999   999   999   999   999   999   5     5     999   999   5     0  
+0     27.2  999   999   999   999   999   999   999   999   999   999   999   999
+27.2  0     29.8  999   999   999   100.9 999   999   999   999   999   999   999
+999   29.8  0     31.7  68.1  999   999   999   999   999   999   999   999   999
+999   999   31.7  0     999   999   999   999   999   999   999   999   999   999
+999   999   68.1  999   0     33.7  999   999   82.1  999   999   999   999   999
+999   999   999   999   33.7  0     999   999   999   999   999   999   999   999
+999   100.9 999   999   999   999   0     33.7  999   82.1  999   999   999   999
+999   999   999   999   999   999   33.7  0     999   999   999   999   999   999
+999   999   999   999   82.1  999   999   999   0     999   999   999   99.3  176.5
+999   999   999   999   999   999   82.1  999   999   0     69.1  999   999   176.5
+999   999   999   999   999   999   999   999   999   69.1  0     29.1  30.1  999
+999   999   999   999   999   999   999   999   999   999   29.1  0     999   999
+999   999   999   999   999   999   999   999   99.3  999   30.1  999   0     74.5
+999   999   999   999   999   999   999   999   176.5 176.5 999   999   74.5     0
 */
