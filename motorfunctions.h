012355fa70d363af8866d849b0d41dc74c44269c
Rohan Babu <115554587+Airborne343@users.noreply.github.com>
2024-02-06 07:23:50 +0000

    Update motorfunctions.h

--- motorfunctions.h
+++ motorfunctions.h
@@ -1,338 +1,138 @@
-#include "dijkstra.h"
-#include "linetracking_timedelay.h"
-//NOTE TO SELF: VOID NODES WHICH MIGHT NEED CHANGE -> 23, 32, 45, 67...
-int array_size = 0;
-int i =0;
+#include <Adafruit_MotorShield.h>
+#define MAX_RANG (520)//the max measurement value of the module is 520cm(a little bit longerthan effective max range)
+#define ADC_SOLUTION (1023.0)//ADC accuracy of Arduino UNO is 10bit
 
-void node01(){
-  initialise();
-  linetrack = true;
-  while (linetrack == true){
-    svr=digitalRead(sr);
-    svl=digitalRead(sl);
-    svvr=digitalRead(ssr);
-    svvl=digitalRead(ssl);
-    if (svvl==HIGH && svvr == HIGH ){
-      stop();
-      Serial.println("Stop 01");
-      linetrack = false;
-    }
-  }
-  delay(1000);
-}
-void node10(){
-  
-  if (path[i+1] == 2){
-    right90();
-    linetrack = true;
-    if (svvl==HIGH && svvr == HIGH){
-      stop();
-    }
-    linetrack = false;
-    forward();
-    delay(1000);
-  }
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *LeftMotor = AFMS.getMotor(4);
+Adafruit_DCMotor *RightMotor = AFMS.getMotor(1);
 
-  else if (path[i+1] == 6){
-    left90();
-    linetrack = true;
-    if (svvl==HIGH && svvr == HIGH){
-      stop();
-    }
-    linetrack = false;
-    forward();
-    delay(1000);
-  } 
-}
-void node12(){
-  // if (path[i+1] == 0){
-    Serial.println("Node 12");
-    forward();
-    delay(700);
-    left90();
-    linetrack = true;
-    if (svvr == HIGH){
-      stop();
-    }
-    linetrack = false;
-    delay(1000);
-  // }
-  
-  // else if (path[i+1] == 6){
-  //   linetrack = true;
-  //   if (svvr == HIGH){
-  //     stop();
-  //   }
-  //   linetrack = false;
-  //   delay(1000);
-  // }
-}
-void node16(){
-  if (path[i+1] == 0){
-    right90();
-    linetrack = true;
-    if (svvl==HIGH && svvr == HIGH){
-      stop();
-    }
-    linetrack = false;
-    delay(1000);
-  }
 
-  if (path[i+1] == 2){
-    linetrack = true;
-    if (svvl==HIGH && svvr == HIGH){
-      stop();
-    }
-    linetrack = false;
-    delay(1000);
-  }
+int sensityPin = A0; // select the input pin
+float dist_t1;
+float sensity_t1; 
+float dist_t3;
+float sensity_t3, dist_t2, sensity_t2, dist_t ;// define variables
+bool Soft = false; //define block
+int tdelay=2; 
+
+float ultrasensor_reading(){
+   // read the value from the sensor:
+sensity_t1 = analogRead(sensityPin);
+dist_t1 = sensity_t1 * MAX_RANG / ADC_SOLUTION;//
+delay(10);
+sensity_t2 = analogRead(sensityPin);
+dist_t2 = sensity_t2 * MAX_RANG / ADC_SOLUTION;//
+delay(10);
+sensity_t3 = analogRead(sensityPin);
+dist_t3 = sensity_t3 * MAX_RANG / ADC_SOLUTION;//
+delay(10);
+
+dist_t = (dist_t1+dist_t2+dist_t3)/3;
+Serial.println(dist_t);
+return dist_t;
+
 }
-void node21(){
-  if (path[i+1] == 3){
-    right90();
-    linetrack = true;
-    if(svvr == HIGH){
-      stop();
-    }
-    linetrack = false;
-    delay(1000);
+
+void ultrasensor(float distance) {
+if(1<distance && distance< 5.6)
+  {
+    Serial.println("hard");
+    Serial.println(distance);
+    Soft = false;
+    //counter ++; //metal block detect so go to green 
+    delay(500);
+  }
+  // else if (6 <dist_t && dist_t < 9.5)   
+  //     {
+  //     Serial.println("soft soft");
+  //     Serial.println(dist_t);
+  //    Soft = true;
+  //    //counter += 2; //foam block detect so go to red
+  //    delay(500);
+  //    }
+else {
+  Serial.println("soft");
+  Serial.println(distance);
+  Soft = true;
+    //counter += 2; //foam block detect so go to red
+  delay(500);
+}
+Serial.print("sense");
+delay(3000);
+}
+
+void forward()
+ {
+  //adjust the speed here if needed
+  LeftMotor->run(FORWARD);
+  RightMotor->run(FORWARD);
+  Serial.println("FORWARD");
+  delay(tdelay);
+ } 
+
+void backward()
+   {
+    LeftMotor->run(BACKWARD);
+    RightMotor->run(BACKWARD);
+    Serial.println("BACKWARD");
+    delay(tdelay);
+   }
+
+void right() 
+ {
+  LeftMotor->setSpeed(225);
+  LeftMotor->run(FORWARD);
+  RightMotor->setSpeed(25);
+  RightMotor->run(FORWARD);
+  Serial.println("RIGHT");
+  delay(tdelay);
   }
 
-  else if (path[i+1] == 4){
-    linetrack = true;
-    if(svvr == HIGH){
-      stop();
-    }
-    linetrack = false;
-    delay(1000);
-  }
-}
-void node23(){
-  // if (path[i+1] == 1){
-    forward();
-    delay(700);
-    right90();
-    linetrack = true;
-    if (svl == LOW && svr == LOW){
-      stop();
-    }
-    linetrack = false;
-    delay(1000);}
-  
-  // else if (path[i+1] == 4){
-  //   left90();
-  //   linetrack = true;
-  //   if (svl == LOW && svr == LOW){
-  //     stop();
-  //   }
-  //   linetrack = false;
-  //   delay(1000);}
 
-    //ADD BLOCKDETECTION
-void node24(){
-  if (path[i+1] == 1){
-  linetrack = true;
-  if (svvl == HIGH && svvr == HIGH){
-    stop();
-  }
-  linetrack = false;
-  delay(1000);}
+void left() 
+ {
+  RightMotor->setSpeed(225);
+  RightMotor->run(FORWARD);
+  LeftMotor->setSpeed(25);
+  LeftMotor->run(FORWARD);
+  Serial.println("LEFT");
+  delay(tdelay); 
+}  
 
-  else if (path[i+1] == 3){
-  left90();
-  linetrack = true;
-  if (svvl == HIGH && svvr == HIGH){
-    stop();
-  }
-  linetrack = false;
-  delay(1000);}
-}
-void node32(){
-  //backlinetrack?
-  // backwards();
-  if (svvl == HIGH && svvr == HIGH){
-    stop();
-  }
-  delay(1000);
-}
-void node42(){
-  if (path[i+1] == 5){
-    right90();
-    linetrack = true;
-    if (svvl == HIGH){
-      stop();
-    }
-    linetrack = false;
-    delay(1000);}
-
-  else if (path[i+1] == 8){
-    left90();
-    linetrack = true;
-    if (svvl == HIGH){
-      stop();
-    }
-    linetrack = false;
-    delay(1000);}
-}
-// void node45(){
-//   if (path[i+1] == 2){
-//     left90();
-//     linetrack
-//   }
-// }
-// void node48(){}
-// void node54(){}
-// void node61(){}
-// void node67(){}
-// void node69(){}
-// void node76(){}
-// void node84(){}
-// void node812(){}
-// void node813(){}
-// void node96(){}
-// void node910(){}
-// void node913(){}
-// void node109(){}
-// void node1011(){}
-// void node1012(){}
-// void node1110(){}
-// void node128(){}
-// void node1210(){}
-// void node1213(){}
-// void node138(){}
-// void node139(){}
-// void node1312(){}
-
-int* pathnode(){
-  int array_size = sizeof(path)/sizeof(path[0]);
-  for (int i = array_size - 1; i>=0; i--) {
-    if (path[i] == 0 && path[i-1] == 1){
-    node01();
+void right90() //turn 90 degrees
+ {
+  //unsigned long current_time = millis(); 
+  // Serial.println(current_time);
+  // int interval = 2000;
+  // while((millis()-current_time) < interval){
+  // Serial.println(millis());
+  Serial.println("RIGHT90");
+  forward();
+  delay(500);
+  RightMotor->setSpeed(150);
+  LeftMotor->setSpeed(150);
+  LeftMotor->run(FORWARD);
+  RightMotor->run(BACKWARD);
+    delay(1300);
   }
   
-  else if (path[i] == 1 && path[i-1] == 0){
-    node10();
+
+void left90() //turn 90 degrees
+ {
+  Serial.println("LEFT90");
+  forward();
+  delay(500);
+  RightMotor->setSpeed(150);
+  LeftMotor->setSpeed(150);
+  LeftMotor->run(BACKWARD);
+  RightMotor->run(FORWARD);
+    delay(1300);
   }
 
-  else if (path[i] == 1 && path[i-1] == 2){
-    node12();
-  }
-
-  else if (path[i] == 1 && path[i-1] == 6){
-    node16();
-  }
-
-  else if (path[i] == 2 && path[i-1] == 1){
-    node21();
-  }
-
-  else if (path[i] == 2 && path[i-1] == 3){
-    node23();
-  }
-
-  else if (path[i] == 2 && path[i-1] == 4){
-    node24();
-  }
-
-  else if (path[i] == 3 && path[i-1] == 2){
-    node32();
-  }
-
-  else if (path[i] == 4 && path[i-1] == 2){
-    node42();
-  }
-
-  // else if (path[i] == 4 && path[i-1] == 5){
-  //   node45();
-  // }
-
-  // else if (path[i] == 4 && path[i-1] == 8){
-  //   node48();
-  // }
-
-  // else if (path[i] == 5 && path[i-1] == 4){
-  //   node54();
-  // }
-
-  // else if (path[i] == 6 && path[i-1] == 1){
-  //   node61();
-  // }
-
-  // else if (path[i] == 6 && path[i-1] == 7){
-  //   node67();
-  // }
-
-  // else if (path[i] == 6 && path[i-1] == 9){
-  //   node69();
-  // }
-
-  // else if (path[i] == 7 && path[i-1] == 6){
-  //   node76();
-  // }
-
-  // else if (path[i] == 8 && path[i-1] == 4){
-  //   node84();
-  // }
-
-  // else if (path[i] == 8 && path[i-1] == 12){
-  //   node812();
-  // }
-
-  // else if (path[i] == 8 && path[i-1] == 13){
-  //   node813();
-  // }
-
-  // else if (path[i] == 9 && path[i-1] == 6){
-  //   node96();
-  // }
-
-  // else if (path[i] == 9 && path[i-1] == 10){
-  //   node910();
-  // }
-
-  // else if (path[i] == 9 && path[i-1] == 13){
-  //   node913();
-  // }
-
-  // else if (path[i] == 10 && path[i-1] == 9){
-  //   node109();
-  // }
-
-  // else if (path[i] == 10 && path[i-1] == 11){
-  //   node1011();
-  // }
-
-  // else if (path[i] == 10 && path[i-1] == 12){
-  //   node1012();
-  // }
-
-  // else if (path[i] == 11 && path[i-1] == 10){
-  //   node1110();
-  // }
-
-  // else if (path[i] == 12 && path[i-1] == 8){
-  //   node128();
-  // }
-
-  // else if (path[i] == 12 && path[i-1] == 10){
-  //   node1210();
-  // }
-
-  // else if (path[i] == 12 && path[i-1] == 13){
-  //   node1213();
-  // }
-
-  // else if (path[i] == 13 && path[i-1] == 8){
-  //   node138();
-  // }
-
-  // else if (path[i] == 13 && path[i-1] == 9){
-  //   node139();
-  // }
-
-  // else if (path[i] == 13 && path[i-1] == 12){
-  //   node1312();
-  // }
-}
-  return i;
-  }
+void stop()
+ {
+  LeftMotor->run(RELEASE);
+  RightMotor->run(RELEASE);
+ }
